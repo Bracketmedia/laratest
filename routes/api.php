b446911e79deb8f264c145611130fd3b5fab97da
matias <matyv3@gmail.com>
2019-07-16 21:37:09 -0300

    feat: api rest

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\User;
+use App\Http\Resources\User as UserResource;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,8 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('users', function(){
+    return UserResource::collection(User::paginate());
 });
